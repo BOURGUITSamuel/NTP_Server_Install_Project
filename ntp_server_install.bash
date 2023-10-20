76e1fc11c4cdf9345ae2221d5ef5c47edb519035
Jean - Samuel BOURGUIT <65498199+BOURGUITSamuel@users.noreply.github.com>
2023-10-20 14:23:38 +0200

    Update ntp_server_install.bash

--- ntp_server_install.bash
+++ ntp_server_install.bash
@@ -12,10 +12,12 @@ NTP_CONF_FILE="/etc/ntpsec/ntp.conf"
 # Emplacement du fichier de sauvegarde 
 NTP_BACKUP_DIR="/etc/ntp_backup"
 
-# Vérification si l'utilisateur est root
+# Vérification des privilèges de l'utilisateur
 check_root_user() {
   if [[ $EUID -ne 0 ]]; then
     log_error "Ce script doit être exécuté en tant que root."
+  else 
+    echo "L'utilisateur connecté est bien root." | tee -a "$LOG_FILE"
   fi
 }
 
@@ -24,7 +26,7 @@ function log_error {
     local MESSAGE="$1"
     local TIMESTAMP=$(date +'%Y-%m-%d %H:%M:%S')
     echo "ERREUR [$TIMESTAMP]: $MESSAGE" >&2
-    echo "ERREUR [$TIMESTAMP]: $MESSAGE" >> "$log_file"
+    echo "ERREUR [$TIMESTAMP]: $MESSAGE" >> "$LOG_FILE"
     exit 1
 }
 
@@ -35,7 +37,7 @@ check_ntp_installed() {
 
 # Installation du serveur NTP
 install_ntp() {
-  echo "Installation du serveur NTP..." | tee -a "$LOG_FILE"
+  echo "Installation du serveur NTP en cours..." | tee -a "$LOG_FILE"
   apt-get -y update > /dev/null
   apt-get -y install ntpsec ntpdate > /dev/null
 }
@@ -54,25 +56,41 @@ check_ntp_installation_success() {
 configure_ntp() {
   echo "Configuration du fichier ntp.conf en cours..." | tee -a "$LOG_FILE"
   if grep -i "pool 0.debian.pool.ntp.org iburst" $NTP_CONF_FILE > /dev/null; then
+      echo "Le serveur NTP utilise le pool NTP Debian 00."| tee -a "$LOG_FILE"
+      echo "Ajout du serveur de temps NTP français 00 en cours..."| tee -a "$LOG_FILE"
       sed -i "s/pool 0.debian.pool.ntp.org iburst/server 0.fr.pool.ntp.org/g" $NTP_CONF_FILE
+  elif grep -v '^ *#' $NTP_CONF_FILE | grep -i "server 0.fr.pool.ntp.org" > /dev/null; then
+      echo "Le serveur NTP utilise bien le pool NTP FR 00." | tee -a "$LOG_FILE"
   else    
       log_error "La configuration du paramètre a échoué."
   fi
 
   if grep -i "pool 1.debian.pool.ntp.org iburst" $NTP_CONF_FILE > /dev/null; then
+      echo "Le serveur NTP utilise le pool NTP Debian 01." | tee -a "$LOG_FILE"
+      echo "Ajout du serveur de temps NTP français 01 en cours..." | tee -a "$LOG_FILE"
       sed -i "s/pool 1.debian.pool.ntp.org iburst/server 1.fr.pool.ntp.org/g" $NTP_CONF_FILE
+  elif grep -v '^ *#' $NTP_CONF_FILE | grep -i "server 1.fr.pool.ntp.org" > /dev/null; then
+      echo "Le serveur NTP utilise bien le pool NTP FR 01." | tee -a "$LOG_FILE"
   else
       log_error "La configuration du paramètre a échoué."    
   fi
   
   if grep -i "pool 2.debian.pool.ntp.org iburst" $NTP_CONF_FILE > /dev/null; then
+      echo "Le serveur NTP utilise le pool NTP Debian 02." | tee -a "$LOG_FILE"
+      echo "Ajout du serveur de temps NTP français 02 en cours..." | tee -a "$LOG_FILE"
       sed -i "s/pool 2.debian.pool.ntp.org iburst/server 2.fr.pool.ntp.org/g" $NTP_CONF_FILE
+  elif grep -v '^ *#' $NTP_CONF_FILE | grep -i "server 2.fr.pool.ntp.org" > /dev/null; then
+      echo "Le serveur NTP utilise bien le pool NTP FR 02." | tee -a "$LOG_FILE"
   else
       log_error "La configuration du paramètre a échoué."
   fi
 
   if grep -i "pool 3.debian.pool.ntp.org iburst" $NTP_CONF_FILE > /dev/null; then
+      echo "Le serveur NTP utilise le pool NTP Debian 03." | tee -a "$LOG_FILE"
+      echo "Ajout du serveur de temps NTP français 03 en cours..." | tee -a "$LOG_FILE"
       sed -i "s/pool 3.debian.pool.ntp.org iburst/server 3.fr.pool.ntp.org/g" $NTP_CONF_FILE
+  elif grep -v '^ *#' $NTP_CONF_FILE | grep -i "server 3.fr.pool.ntp.org" > /dev/null; then
+      echo "Le serveur NTP utilise bien le pool NTP FR 03." | tee -a "$LOG_FILE"
   else
       log_error "La configuration du paramètre a échouée."
   fi 
@@ -113,7 +131,7 @@ restart_ntp_service() {
 
 # Vérification du redémarrage du service NTP
 check_ntp_service_status() {
-  echo "Vérification du redémarrage du service NTP..." | tee -a "$LOG_FILE"
+  echo "Vérification du redémarrage du service NTP en cours..." | tee -a "$LOG_FILE"
   if ! systemctl is-active ntpsec > /dev/null; then
     log_error "Le service NTP n'a pas redémarré correctement."
   else
@@ -123,18 +141,18 @@ check_ntp_service_status() {
 
 # Sauvegarde du fichier de configuration avec horodatage
 backup_ntp_config() {
-  echo "Sauvegarde du fichier de configuration..." | tee -a "$LOG_FILE"
-  mkdir -p "$NTP_BACKUP_DIR" > /dev/null
-  cp "$NTP_CONF_FILE" "$NTP_BACKUP_DIR/ntp.conf_${current_date}.bak" > /dev/null
-}
-
-# Vérification de la réussite de la sauvegarde
-check_backup_success() {
-  echo "Vérification de la sauvegarde..." | tee -a "$LOG_FILE"
   if [ -f "$NTP_BACKUP_DIR/ntp.conf_$current_date.bak" ]; then
-    echo "La sauvegarde a réussi." | tee -a "$LOG_FILE"
+    echo "Le fichier de sauvegarde existe déjà." | tee -a "$LOG_FILE"
   else
+    echo "Sauvegarde du fichier de configuration..." | tee -a "$LOG_FILE"
+    mkdir -p "$NTP_BACKUP_DIR" > /dev/null
+    cp "$NTP_CONF_FILE" "$NTP_BACKUP_DIR/ntp.conf_${current_date}.bak" > /dev/null
+    echo "Vérification de la sauvegarde..." | tee -a "$LOG_FILE"
+    if [ -f "$NTP_BACKUP_DIR/ntp.conf_$current_date.bak" ]; then
+      echo "La sauvegarde a réussi." | tee -a "$LOG_FILE"
+    else
     log_error "La sauvegarde a échoué."
+    fi
   fi
 }
 
@@ -151,7 +169,6 @@ fi
 
 check_ntp_installation_success
 backup_ntp_config
-check_backup_success
 configure_ntp
 check_ntp_connectivity
 enable_ntp_at_startup
